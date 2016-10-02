c2067970e3d1c23581c0d21f607019cee6884fa5
risatino <charissatolentino@gmail.com>
2016-10-02 07:30:13 -0500

    all sorts of attempts

--- algo/prime_number.rb
+++ algo/prime_number.rb
@@ -1,17 +1,52 @@
 # Use RSpec to test the following exercise! You may wish to wrap the methods below in a class to help facilitate this.
 
+require 'rspec'
+
 # First, implement the is_prime? method below. It should accept a number as an argument and return
 # true if the number is prime and false if it is not. A prime number is a number that is only
 # divisible by itself and the number 1.
 
-def is_prime?(number)
-
-end
+class Generator
+  def is_prime?(number)
+    if is_prime?(number) == true
+      return true
+    else
+      return false
+    end
+  end
 
 # Second, implement the highest_prime_number_under method below. It should accept a number as
 # an argument and return the highest prime number under that number. For example, the 
 # highest prime number under 10 is 7.
 
-def highest_prime_number_under(number)
+  def highest_prime_number_under(number)
+    all_numbers = []
+    number.times do
+      number = number - 1
+      all_numbers << number
+    end
+    all_numbers.each do |one_number|
+      if is_prime?(one_number)
+        return one_number
+        break
+      end
+    end
+  end
+end
 
+RSpec.describe Generator do
+  let(:generator) { Generator.new }
+
+  describe '#is_prime?' do
+    it 'should return true if number is prime' do
+      expect(Generator.is_prime?(5)).to eq(true)
+    end
+  end
+
+  describe '#highest_prime_number_under' do
+    it 'should return the highest prime number under the number given' do
+      expect(generator.highest_prime_number_under(10)).to eq(7)
+    end
+  end
+  
 end
--- oop/thesaurus.rb
+++ oop/thesaurus.rb
@@ -12,3 +12,70 @@
 # and which belongs in the Entry class.
 #
 # And... test your functionality using RSpec!
+
+require 'rspec'
+
+class Thesaurus
+  def initialize(input)
+    @words = []
+    @words << input [:word]
+    @synonyms = []
+    @synonyms << input[:synonym]
+    @antonyms = []
+    @antonyms << input[:antonym]
+  end
+
+  def search_synonym(word)    
+  # I keep muddling this section with all sorts of stuff >.< help
+    @synonyms = []
+    @synonyms << input[:synonym]
+  end
+
+  def search_antonym(word)
+    # I am stuck and pretty much lost.
+    @antonyms = []
+    @antonyms << input[:antonym]
+  end
+end
+
+class Entry < Thesaurus
+  def add_new_word(word)
+    @words << word
+    return @words
+  end
+
+  def delete_word(word)
+    @words.delete(word)
+    return @words
+  end
+end
+
+RSpec.describe Thesaurus do
+  describe '#add_new_word' do
+    it 'should return a word if given a word' do
+      thesaurus = Entry.new(word: "sleepy", synonym: "tired")
+      expect(thesaurus.add_new_word("pingpong")).to eq(["sleepy", "pingpong"])
+    end
+  end
+  describe '#delete_word' do
+    it 'should delete the word given' do
+      thesaurus = Entry.new(word: "sleepy")
+      thesaurus.add_new_word("pingpong")
+      thesaurus.add_new_word("lazy")
+      expect(thesaurus.delete_word("lazy")).to eq(["sleepy", "pingpong"])
+    end
+  end
+  describe '#search_synonym' do
+    it 'should search the synonym for word' do
+      thesaurus = Entry.new(word: "sleepy", synonym: "tired")
+      thesaurus.add_new_word("pingpong")
+      expect(thesaurus.search_synonym("sleepy")).to eq("tired")
+    end
+  end
+  describe '#search_antonym' do
+    it 'should search the antonym for word' do
+      thesaurus = Entry.new(word: "sleepy", synonym: "tired", antonym: "awake")
+      expect(thesaurus.search_antonym("sleepy")).to eq("awake")
+    end
+  end
+end
